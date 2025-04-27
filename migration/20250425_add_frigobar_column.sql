e8ddf016aee5c8b46e8a5263f031ae7e529741f8
Felipe Estevam <estevamx9@gmail.com>
2025-04-27 12:28:24 -0300

    Adição de Espécie no Banco de Dados
    feat: Adição de uma coluna de especie no banco de dados, tabelas (clientes, funcionarios, quartos(.

--- migration/20250425_add_frigobar_column.sql
+++ migration/20250425_add_frigobar_column.sql
@@ -1,3 +1,7 @@
 SELECT * FROM `frigobar` WHERE 1
  ALTER TABLE frigobar ADD valor_total DECIMAL(10,2) NOT NULL DEFAULT 0.00;
 
+ ALTER TABLE `clientes` ADD `especie` VARCHAR(100) NOT NULL ; 
+ ALTER TABLE `funcionarios` ADD `especie` VARCHAR(100) NOT NULL ; 
+ ALTER TABLE `quartos` ADD `especie` VARCHAR(100) NOT NULL ; 
+
